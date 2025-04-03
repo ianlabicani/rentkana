ebebceaf4c4adfecc3184e71f82ea8e4bc6a25c0
ianlabicani <labicaniian@gmail.com>
2025-04-03 20:35:49 +0800

    add route resource for rooms

--- routes/landlord.php
+++ routes/landlord.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\Landlord\DashboardController;
+use App\Http\Controllers\Landlord\RoomController;
 use Illuminate\Support\Facades\Route;
 Route::prefix('landlord')->name('landlord.')->middleware(['auth', 'role:landlord'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::resource('rooms', RoomController::class);
 });
