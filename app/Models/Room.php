47da767ff1086f8deee599ab8fcff2f58edd30a2
ianlabicani <labicaniian@gmail.com>
2025-04-03 20:34:26 +0800

    add Room model and establish relationship with User model

---
+++ app/Models/Room.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    use HasUuids;
+
+    protected $fillable = [
+        'landlord_id',
+        'title',
+        'description',
+        'price',
+        'location',
+        'status',
+    ];
+
+    public function landlord()
+    {
+        return $this->belongsTo(User::class, 'landlord_id');
+    }
+
+}
