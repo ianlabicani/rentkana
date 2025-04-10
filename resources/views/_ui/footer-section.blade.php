2de9e3d8f2776a248ea211cd1a5cdebfc55a3e2c
narra <ermanfaminiano020@gmail.com>
2025-04-11 06:47:02 +0800

    feat: add navbar and footer sections for improved layout and user experience

---
+++ resources/views/_ui/footer-section.blade.php
@@ -0,0 +1,44 @@
+<footer class="bg-navbar footer-text py-4">
+    <div class="container">
+        <div class="row align-items-center text-center text-md-start">
+            <div class="col-md-4 mb-3 mb-md-0">
+                <div class="d-flex flex-column justify-content-center justify-content-md-start align-items-center align-items-md-start">
+                    <div class="logo-container">
+                        <img src="{{ asset('images/png/logo2.png') }}" alt="logo" height="40" class="me-3">
+                    </div>
+                    <div class="small">© 2025 Rentkana, Inc.<br>All rights reserved.</div>
+                </div>
+            </div>
+
+            <div class="col-md-2 mb-3 mb-md-0">
+                <h6 class="text-uppercase mb-3">Company</h6>
+                <ul class="list-unstyled mb-0">
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">About</a></li>
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">FAQ</a></li>
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">Blog</a></li>
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">Careers</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 mb-3 mb-md-0">
+                <h6 class="text-uppercase mb-3">Explore</h6>
+                <ul class="list-unstyled mb-0">
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">Room Types</a></li>
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">Locations</a></li>
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">Pricing</a></li>
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">Partner Landlords</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 mb-3 mb-md-0">
+                <h6 class="text-uppercase mb-3">Connect</h6>
+                <ul class="list-unstyled mb-0">
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">Facebook</a></li>
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">Twitter</a></li>
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">Instagram</a></li>
+                    <li class="mb-2"><a href="#" class="text-black text-decoration-none">LinkedIn</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
