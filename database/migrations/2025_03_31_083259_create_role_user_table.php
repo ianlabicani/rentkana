61c20c831df2ba747e83af0e0971baa8122ab750
ianlabicani <labicaniian@gmail.com>
2025-06-20 09:31:42 +0800

    update deafult for unique

--- database/migrations/2025_03_31_083259_create_role_user_table.php
+++ database/migrations/2025_03_31_083259_create_role_user_table.php
@@ -5,15 +5,14 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->uuid('id')->primary()->default(value: DB::raw(DB::raw('(UUID())')));
+            $table->uuid('id')->primary();
             $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignUuid('role_id')->constrained('roles')->onDelete('cascade');
             $table->timestamps();
